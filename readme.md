b29bdd51d839cec9ff185f7a9b5ba14ca6f64da6
R-Dineshbabu <dineshsachin393@gmail.com>
2023-12-01 18:46:03 +0530

    completed

---
+++ readme.md
@@ -0,0 +1,3 @@
+Displaying countdown from 1 to 10 on the screen and then display
+Happy independence day On the screen acheived by call back hell 
+and settimeout
---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+  document.getElementById("project").innerText="10"
+  setTimeout(() => {
+    document.getElementById("project").innerText="9"
+    setTimeout(() => {
+      document.getElementById("project").innerText="8"
+      setTimeout(() => {
+        document.getElementById("project").innerText="7"
+        setTimeout(() => {
+          document.getElementById("project").innerText="6"
+          setTimeout(() => {
+            document.getElementById("project").innerText="5"
+            setTimeout(() => {
+              document.getElementById("project").innerText="4"
+              setTimeout(() => {
+                document.getElementById("project").innerText="3"
+                setTimeout(() => {
+                  document.getElementById("project").innerText="2"
+                  setTimeout(() => {
+                    document.getElementById("project").innerText="1"
+                    setTimeout(() => {
+                      document.getElementById("project").innerText="HAPPY INDEPENDENCE DAY🫡"
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
